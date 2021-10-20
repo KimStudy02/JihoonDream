a43f389cecbcdb9c09baf75dbbaecde8cbc0e278
DESKTOP-S31E8UC\KimJihoon <aodtn11@naver.com>
2021-10-20 21:28:01 +0900

    Project Upload

---
+++ JiHoonTower/Assets/Script/TowerBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : MonoBehaviour
+{
+    public SpriteRenderer towerBody;
+    public SpriteRenderer towerRoof;
+
+    private void Awake()
+    {
+        //int towerBodyLayerID = SortingLayer.NameToID("TowerBody");
+        //int towerRoofLayerID = SortingLayer.NameToID("TowerRoof");
+        //towerBody.sortingLayerID = SortingLayer.GetLayerValueFromID(towerBodyLayerID);
+        //towerRoof.sortingLayerID = SortingLayer.GetLayerValueFromID(towerRoofLayerID);
+
+        towerBody.sortingLayerName = "TowerBody";
+        towerRoof.sortingLayerName = "TowerRoof";
+    }
+}
---
+++ JiHoonTower/Assets/Study/SutdyScript/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int             wayPointCount;
+    public Transform[]     wayPoints;
+    public int             currentIndex = 0;
+    public Movement2D      movement2D;
+
+    public void Setup(Transform[] wayPoints)
+    {
+        movement2D          = GetComponent<Movement2D>();
+
+        wayPointCount       = wayPoints.Length;
+        this.wayPoints      = new Transform[wayPointCount];
+        this.wayPoints      = wayPoints;
+
+        transform.position = wayPoints[currentIndex].position;
+
+        NextMoveTo();
+        //StartCoroutine(OnMove());
+    }
+
+    void Update()
+    {
+        
+
+        if (Vector3.Distance(transform.position, wayPoints[currentIndex].position) <= Time.deltaTime * movement2D.MoveSpeed)
+        {
+            NextMoveTo();
+        }
+
+        transform.Rotate(Vector3.forward * 10);
+
+        if (Vector3.Distance(transform.position, wayPoints[currentIndex].position) <= Time.deltaTime * movement2D.MoveSpeed)
+        {
+            NextMoveTo();
+        }
+    }
+
+    private void NextMoveTo()
+    {
+        if(currentIndex < wayPointCount - 1)
+        {
+            transform.position = wayPoints[currentIndex].position;
+
+            currentIndex++;
+            Vector3 direction = (wayPoints[currentIndex].position - transform.position).normalized;
+            movement2D.MoveTo(direction);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
